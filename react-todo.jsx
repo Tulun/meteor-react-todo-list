e607aebfe3feddc6e787783dc5749c6eb486a3a6
Tulun <jaykiraly@gmail.com>
2016-02-05 18:09:44 -0800

    Added first React components.

---
+++ react-todo.jsx
@@ -0,0 +1,10 @@
+if (Meteor.isClient) {
+  // Client side JS.
+
+  Meteor.startup(function() {
+    // Use Meteor.startup to render the component after the page is ready.
+    React.render(<App />, document.getElementById('render-target'));
+  });
+
+  
+}
