c163cf4d5fc0f56e8510ceb9f92609c3e123cf97
Tulun <jaykiraly@gmail.com>
2016-02-05 19:54:04 -0800

    Added accounts ui to React.

--- react-todo.jsx
+++ react-todo.jsx
@@ -4,6 +4,10 @@ Tasks = new Mongo.Collection('tasks');
 if (Meteor.isClient) {
   // Client side JS.
 
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
   Meteor.startup(function() {
     // Use Meteor.startup to render the component after the page is ready.
     ReactDOM.render(<App />, document.getElementById('render-target'));
